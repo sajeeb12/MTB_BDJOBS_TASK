ed71aad8ab60ba2025482c09bd96ea06f14d25b8
sajeeb12 <shantoislamsajeeb@gmail.com>
2024-09-22 15:44:49 +0600

    canActivateAuthGuard

--- src/app/components/main-form/main-form.component.ts
+++ src/app/components/main-form/main-form.component.ts
@@ -20,12 +20,9 @@ export class MainFormComponent {
 
   formValue:any;
   onSave(){
-    // this.formValue = this.personForm.value;
      if (this.personForm.invalid) {
-      // Mark all controls as touched if the form is invalid, so error messages show up
       this.personForm.markAllAsTouched();
     } else {
-      // Process the form when valid (for example, log form data to console)
       this.checkCredentials();
     }
   }
@@ -33,7 +30,7 @@ export class MainFormComponent {
   checkCredentials(){
     if(this.personForm.get('loginId')?.value == this.initializedLoginId && 
       this.personForm.get('pass')?.value == this.initializedPass){
-        alert('Login Successfull');
+        // alert('Login Successfull');
         localStorage.setItem('loginUser',this.personForm.get('loginId')?.value)
         this.router.navigateByUrl('instruction');
       }else{
